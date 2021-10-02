40c38913ddb40311ece6b06a15f2bf534b280d66
CaOH <caolier@appublicar.com>
2021-10-02 12:46:12 -0500

    bug pagination

--- resources/views/livewire/covid-global-data.blade.php
+++ resources/views/livewire/covid-global-data.blade.php
@@ -110,15 +110,12 @@
                 </div> 
             </div>
             {{-- end:navbar --}}
-              
-              
-
               <div>@php $paginator= $CovidDataTable @endphp
                    @if ($paginator->hasPages())
                        <nav role="navigation" aria-label="Pagination Navigation" class="flex justify-between">
                            <span>
                                {{-- Previous Page Link --}}
-                               @if ($paginator->onFirstPage())
+                               @if ($page<1)
                                    <span class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 cursor-default leading-5 rounded-md">
                                        {!! __('pagination.previous') !!}
                                    </span>
@@ -131,7 +128,8 @@
                 
                            <span>
                                {{-- Next Page Link --}}
-                               @if ($paginator->hasMorePages())
+                               @if(count($CovidDataData)==$CovidDataTable->perPage())
+                               {{-- @if ($paginator->hasMorePages()) --}}
                                    <button wire:click="$set('page',{{$page+1}})"  rel="next" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
                                        {!! __('pagination.next') !!}
                                    </button>
@@ -166,7 +164,7 @@
                   <tbody>
                     @forelse ($CovidDataData as $index => $CountryData)
                     <tr>
-                      <th>{{$index}}</th> 
+                      <th>{{$index+1}}</th> 
                       <td>{{$CountryData['country']}}</td> 
                       <td>{{$CountryData['cases']}}</td> 
                       <td>{{$CountryData['todayCases']}}</td> 
